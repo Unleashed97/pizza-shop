f9dc15fe74984cbb27027d632697b168431ee39a
Nichkov Roman <nichkovroman@gmail.com>
2021-10-01 14:27:05 +0300

    nav items check for undefined, btns replaced to comp

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './Nav.scss'
 
@@ -6,14 +6,30 @@ import NavSorting from '../NavSorting/NavSorting'
 import Button from '../Button/Button'
 
 const Nav = ({ items }) => {
+    const [selectedCategory, setSelectedCategory] = useState(0)
+
+    const navButtonClickHandler = (index) => {
+        setSelectedCategory(index)
+    }
+
     return (
         <nav className="nav">
             <ul className="nav__list">
-                {items.map((item, index) => (
-                    <li className="nav__item" key={index}>
-                        <Button className="btn--nav">{item}</Button>
-                    </li>
-                ))}
+                {items &&
+                    items.map((item, index) => (
+                        <li className="nav__item" key={item + index}>
+                            <Button
+                                className={
+                                    selectedCategory === index
+                                        ? 'btn--nav active'
+                                        : 'btn--nav'
+                                }
+                                onClick={() => navButtonClickHandler(index)}
+                            >
+                                {item}
+                            </Button>
+                        </li>
+                    ))}
             </ul>
             <NavSorting />
         </nav>
